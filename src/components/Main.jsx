5e5e6d67450eece8e2ee758862f45d89eb0afb57
FirdeusGj <firdegjepali@gmail.com>
2023-08-10 09:10:24 +0200

    fresh start

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <div className="main__body">
+        <div>
+        <h1>Foodify</h1>
+        <h3>Where every bite tells a story</h3>
+        <h5>Foodify is your premier Food Search Engine in which you can discover the recipes you crave, effortlessly.</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
